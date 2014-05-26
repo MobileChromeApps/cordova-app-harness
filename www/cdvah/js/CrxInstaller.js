bdcb22b85c7a1b9eb7f14b0285073508f030252c
Michal Mocny <mmocny@gmail.com>
2014-05-26 16:17:39 -0400

    Use cca code for extracting plugins from manifest

--- www/cdvah/js/CrxInstaller.js
+++ www/cdvah/js/CrxInstaller.js
@@ -33,66 +33,6 @@
         '<content src="plugins/org.chromium.bootstrap/chromeapp.html" />\n' +
         '</widget>\n';
 
-    // TODO: put these constants into an npm module.
-    // TODO: Move CRX support into MobileChromeApps/harness.
-    var DEFAULT_PLUGINS = [
-        'org.apache.cordova.file',
-        'org.apache.cordova.inappbrowser',
-        'org.apache.cordova.network-information',
-        'org.apache.cordova.keyboard',
-        'org.apache.cordova.statusbar',
-        'org.chromium.navigation',
-        'org.chromium.bootstrap',
-        'org.chromium.i18n',
-        'org.chromium.polyfill.CustomEvent',
-        'org.chromium.polyfill.xhr_features',
-        'org.chromium.polyfill.blob_constructor'
-    ];
-
-    var PLUGIN_MAP = {
-      'alarms': ['org.chromium.alarms'],
-      'fileSystem': ['org.chromium.fileSystem',
-                     'org.chromium.FileChooser'],
-      'gcm': ['org.chromium.gcm'],
-      'identity': ['org.chromium.identity'],
-      'idle': ['org.chromium.idle'],
-      'notifications': ['org.chromium.notifications'],
-      'payments': ['com.google.payments'],
-      'power': ['org.chromium.power'],
-      'pushMessaging': ['org.chromium.pushMessaging'],
-      'socket': ['org.chromium.socket'],
-      'storage': ['org.chromium.storage'],
-      'syncFileSystem': ['org.chromium.syncFileSystem'],
-      'unlimitedStorage': []
-    };
-    function extractPluginsFromManifest(manifest) {
-        var permissions = [],
-                plugins = [],
-                i;
-        if (manifest.permissions) {
-            for (i = 0; i < manifest.permissions.length; ++i) {
-                if (typeof manifest.permissions[i] === 'string') {
-                    var matchPatternParts = /<all_urls>|([^:]+:\/\/[^\/]+)(\/.*)$/.exec(manifest.permissions[i]);
-                    if (!matchPatternParts) {
-                        permissions.push(manifest.permissions[i]);
-                    }
-                } else {
-                    permissions = permissions.concat(Object.keys(manifest.permissions[i]));
-                }
-            }
-        }
-        for (i = 0; i < permissions.length; i++) {
-            var pluginsForPermission = PLUGIN_MAP[permissions[i]];
-            if (pluginsForPermission) {
-                for (var j = 0; j < pluginsForPermission.length; ++j) {
-                    plugins.push(pluginsForPermission[j]);
-                }
-            } else {
-                console.warn('Permission not supported: ' + permissions[i] + ' (skipping)');
-            }
-        }
-        return DEFAULT_PLUGINS.concat(plugins);
-    }
 
     function generateConfigXmlData(appId, manifest) {
         return CONFIG_XML_TEMPLATE
@@ -102,7 +42,6 @@
             .replace(/__DESCRIPTION__/, (manifest.description) || 'Missing description')
             .replace(/__AUTHOR__/, (manifest.author) || 'Missing author');
     }
-
 
     /* global myApp */
     myApp.factory('CrxInstaller', ['$q', 'Installer', 'AppsService', 'ResourcesLoader', function($q, Installer, AppsService, ResourcesLoader) {
@@ -174,7 +113,7 @@
         };
 
         CrxInstaller.prototype.getPluginMetadata = function() {
-            var pluginIds = extractPluginsFromManifest(this.manifestJson_);
+            var pluginIds = cca.extractPluginsFromManifest(this.manifestJson_);
             var harnessPluginMetadata = cordova.require('cordova/plugin_list').metadata;
             var ret = {};
             // Make all versions match what is installed.
